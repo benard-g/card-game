283c841e7fe85011675a4c3afe9cddd0b2dc86f0
Guillaume Bénard <guillaume@benard.io>
2021-01-17 14:51:56 +0100

    Fix: cookie "sameSite" policy

--- packages/backend/src/api/utils.ts
+++ packages/backend/src/api/utils.ts
@@ -20,3 +20,7 @@ export function getRequestId(res: Response): string {
 export function getRequestingUser(res: Response): User {
   return res.locals[LOCAL_REQUESTING_USER_KEY];
 }
+
+export function setCookie(res: Response, key: string, value: string): void {
+  res.cookie(key, value, { sameSite: 'none' });
+}
